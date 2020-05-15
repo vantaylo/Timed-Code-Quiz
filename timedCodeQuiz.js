0c15501cf05d89cc0255d238bc4547da9bafaf6c
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-05-15 18:11:18 -0500

    js for inputing questions and answers in divs

--- timedCodeQuiz.js
+++ timedCodeQuiz.js
@@ -6,28 +6,34 @@
 // 6. Can save my initials and score
 
 //Variables I will need
-var startBtn = $("#start-btn");
-var quizQuestion = $("#questions");
-var questionContainer = $("#question-container");
-var answerBtns = $("#answer-buttons");
-
-var gameStart = false;
-var gameFinished = false;
-var currentQuestion = 0;
-var score = 0;
-
-function loadQuestion (questionIndex) {
-  var choiceA = $("#choiceA");
-  var choiceB = $("#choiceB");
-  var choiceC = $("#choiceC");
-  var choiceD = $("#choiceD");
-
-  question-container.textContent = (question)
-};
-
 //What should happen when I start
 $(document).ready(function () {
   console.log("ready!");
+  var startBtn = $("#start-btn");
+  var quizQuestion = $("#questions");
+  var questionContainer = $("#question-container");
+  var answerBtns = $("#answer-buttons");
+
+  var gameStart = false;
+  var gameFinished = false;
+  var currentQuestion = 0;
+  var score = 0;
+
+  function showQuestion(questionIndex) {
+    var choiceA = $("#choiceA");
+    var choiceB = $("#choiceB");
+    var choiceC = $("#choiceC");
+    var choiceD = $("#choiceD");
+
+    var selectedQuestion = questions[currentQuestion];
+    questionContainer.text(selectedQuestion.question);
+    console.log("showQuestion");
+
+    choiceA.text(selectedQuestion.answers.A.text);
+    choiceB.text(selectedQuestion.answers.B.text);
+    choiceC.text(selectedQuestion.answers.C.text);
+    choiceD.text(selectedQuestion.answers.D.text);
+  }
 
   $("#start-btn").click(function () {
     console.log("btn");
@@ -44,57 +50,53 @@ $(document).ready(function () {
     console.log();
 
     //render questions
-    var firstQuestion = questions[currentQuestion];
     showQuestion(currentQuestion);
-
   });
 });
 
-
 //selecting an answer function
-  //answer is right
-    //move to next question
-  
-  //answer is wrong
+//answer is right
+//move to next question
 
+//answer is wrong
 
 //Game is over
-
 
 //Questions
 var questions = [
   {
-    question:"What is the HTML tag under which one can write the JavaScript code?",
+    question:
+      "What is the HTML tag under which one can write the JavaScript code?",
     answers: {
-      A: {text: "<javascript>", correct: false},
-      B: {text: "<scripted>", correct: false },
-      C: {text: "<script>", correct: true },
-      D: {text: "<js>", correct: false },
+      A: { text: "<javascript>", correct: false },
+      B: { text: "<scripted>", correct: false },
+      C: { text: "<script>", correct: true },
+      D: { text: "<js>", correct: false },
     },
-  }
+  },
 
   {
-    question: "Which of the following is the correct syntax to display “CodingIsFun” in an alert box using JavaScript?",
+    question:
+      "Which of the following is the correct syntax to display “CodingIsFun” in an alert box using JavaScript?",
     answers: [
       { text: "alertbox(“CodingIsFun”);", correct: false },
       { text: "msg(“CodingIsFun”);", correct: false },
       { text: "msgbox(“CodingIsFun”);", correct: false },
       { text: "alert(“CodingIsFun”);", correct: true },
     ],
-    }
-      
-    {
-    question:"What is the correct syntax for referring to an external script called “main.js”?",
+  },
+
+  {
+    question:
+      "What is the correct syntax for referring to an external script called “main.js”?",
     answers: [
-    { text: "<script src=”main.js”>", correct: true },
-    { text: "<script href=”main.js”>", correct: false },
-    { text: "<script ref=”main.js”>", correct: false },
-    { text: "<script name=”main.js”>", correct: false },
+      { text: "<script src=”main.js”>", correct: true },
+      { text: "<script href=”main.js”>", correct: false },
+      { text: "<script ref=”main.js”>", correct: false },
+      { text: "<script name=”main.js”>", correct: false },
     ],
-  }
+  },
 ];
-
-
 
 // var showQuestion = function (questionIndex) {
 //   var selectedQuestion = questions[questionIndex];
