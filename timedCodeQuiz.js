7d7a5bba4bfb3b1ec66c30af942bc3509d1a4445
Vanessa Taylor <vanessalyn.taylor@gmail.com>
2020-05-15 17:43:47 -0500

    variables and function for loading questions

--- timedCodeQuiz.js
+++ timedCodeQuiz.js
@@ -1,17 +1,33 @@
-//select elements variables
-var start = $("#start-btn");
-var quiz = $("#questions");
-var nextBtn = $("#next-btn");
+// 1. Mulitple choice quiz
+// 2. With timer for each questions
+// 3. Flows through each question automatically once answered or time is up
+// 4. If wrong answer picked deduct 2 seconds
+// 5. When all questions are answered the game is over
+// 6. Can save my initials and score
+
+//Variables I will need
+var startBtn = $("#start-btn");
+var quizQuestion = $("#questions");
+var questionContainer = $("#question-container");
+var answerBtns = $("#answer-buttons");
 
 var gameStart = false;
 var gameFinished = false;
 var currentQuestion = 0;
+var score = 0;
 
+function loadQuestion (questionIndex) {
+  var choiceA = $("#choiceA");
+  var choiceB = $("#choiceB");
+  var choiceC = $("#choiceC");
+  var choiceD = $("#choiceD");
 
+  question-container.textContent = (question)
+};
+
+//What should happen when I start
 $(document).ready(function () {
   console.log("ready!");
-
-  
 
   $("#start-btn").click(function () {
     console.log("btn");
@@ -19,37 +35,36 @@ $(document).ready(function () {
     gameStart = true;
 
     //start the timer
-    // repeat with the interval of 20 seconds
     var timerId = setInterval(function () {
-      console.log("timer");
+      $("#timer").html(i);
+      i++;
     }, 1000);
 
     var sec = 20;
     console.log();
 
     //render questions
-    // var firstQuestion = questions[currentQuestion];
+    var firstQuestion = questions[currentQuestion];
     showQuestion(currentQuestion);
+
   });
 });
 
 
+//selecting an answer function
+  //answer is right
+    //move to next question
+  
+  //answer is wrong
 
 
-var showQuestion = function (questionIndex) {
-  var selectedQuestion = questions[questionIndex];
+//Game is over
 
-  // set html text to show questions
-  $("#-question-box").setText(selectedQuestion.question)
-  $("#A-box").setText(selectedQuestion.answers.A.text)
-
-}
 
 //Questions
 var questions = [
   {
-    question:
-      "What is the HTML tag under which one can write the JavaScript code?",
+    question:"What is the HTML tag under which one can write the JavaScript code?",
     answers: {
       A: {text: "<javascript>", correct: false},
       B: {text: "<scripted>", correct: false },
@@ -57,52 +72,33 @@ var questions = [
       D: {text: "<js>", correct: false },
     },
   }
-  {
-  question:
-    "Which of the following is the correct syntax to display “CodingIsFun” in an alert box using JavaScript?",
-  answers: [
-    { A: "alertbox(“CodingIsFun”);", correct: false },
-    { B: "msg(“CodingIsFun”);", correct: false },
-    { C: "msgbox(“CodingIsFun”);", correct: false },
-    { D: "alert(“CodingIsFun”);", correct: true },
-  ],}
-    
-{
-  question:
-  "What is the correct syntax for referring to an external script called “main.js”?",
-answers: [
-  { A: "<script src=”main.js”>", correct: true },
-  { B: "<script href=”main.js”>", correct: false },
-  { C: "<script ref=”main.js”>", correct: false },
-  { D: "<script name=”main.js”>", correct: false },
-],}
 
+  {
+    question: "Which of the following is the correct syntax to display “CodingIsFun” in an alert box using JavaScript?",
+    answers: [
+      { text: "alertbox(“CodingIsFun”);", correct: false },
+      { text: "msg(“CodingIsFun”);", correct: false },
+      { text: "msgbox(“CodingIsFun”);", correct: false },
+      { text: "alert(“CodingIsFun”);", correct: true },
+    ],
+    }
+      
+    {
+    question:"What is the correct syntax for referring to an external script called “main.js”?",
+    answers: [
+    { text: "<script src=”main.js”>", correct: true },
+    { text: "<script href=”main.js”>", correct: false },
+    { text: "<script ref=”main.js”>", correct: false },
+    { text: "<script name=”main.js”>", correct: false },
+    ],
+  }
 ];
 
-// create some elements
 
-//render a question
-$( "div.demo-container" ).html(function askQuestions(question) {
-  
 
-});
+// var showQuestion = function (questionIndex) {
+//   var selectedQuestion = questions[questionIndex];
 
-//start quiz
-
-//counter rendering
-
-//check answer
-
-//answer is correct
-
-//answer is wrong
-
-// score rendered
-// calculate percent answered correctly
-// keep highscores
-
-// after 1 min stop
-// setTimeout(() => {
-//   clearInterval(timerId);
-//   alert("stop");
-// }, 1000 * 60 * 60);
+//   // set html text to show questions
+//   $("#-question-box").setText(selectedQuestion.question)
+//   $("#A-box").setText(selectedQuestion.answers.A.text)
